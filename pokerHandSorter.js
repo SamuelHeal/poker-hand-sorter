8300d6ea89220d6049ad3aeb6280820a3668561c
Samuel Heal <samuelheal@hotmail.com>
2021-09-09 13:18:48 +1000

    can now determine winner based on 3 of a kind

--- pokerHandSorter.js
+++ pokerHandSorter.js
@@ -520,6 +520,79 @@ function roundWinner(roundData) {
                 }
             }
         }
+
+
+        // ----------------------------------------------------------
+        // Determining three of a kind
+        if (roundContinue) {
+            const playerOneCards = {}
+            let playerOneThreeOfAKind; 
+            var playerOneThreeOfKindCard = 0
+    
+            // goes through player ones cards and sorts it into what card numbers they have, and how many they have, into an object
+            // object will look like this {'1': 4, '5', 1} as an example, with the string being the card number, and the int
+            // being the number of times that card is in the hand
+            playerOneCardNumbers.forEach(function(duplicates) {
+                playerOneCards[duplicates] = (playerOneCards[duplicates] || 0) + 1
+            })
+        
+            // this determines whether the player has any card that appears four times
+            if (playerOneSortedDuplicateCards.length === 3) {
+                for (let i = 0; i < playerOneSortedDuplicateCards.length; i++) {
+                    if (playerOneSortedDuplicateCards[i][1] === 3) {
+                        playerOneThreeOfAKind = true
+                        playerOneThreeOfKindCard = parseInt(playerOneSortedDuplicateCards[i][0])
+                    }
+                }
+            }
+    
+            // above code repeats for player two
+            const playerTwoCards = {}
+            let playerTwoThreeOfAKind; 
+            var playerTwoThreeOfKindCard = 0
+    
+            playerTwoCardNumbers.forEach(function(duplicates) {
+                playerTwoCards[duplicates] = (playerTwoCards[duplicates] || 0) + 1
+            })
+        
+            if (playerTwoSortedDuplicateCards.length === 3) {
+                for (let i = 0; i < playerTwoSortedDuplicateCards.length; i++) {
+                    if (playerTwoSortedDuplicateCards[i][1] === 3) {
+                        playerTwoThreeOfAKind = true
+                        playerTwoThreeOfKindCard = parseInt(playerTwoSortedDuplicateCards[i][0])
+                    }
+                }
+            }
+    
+            // determines who wins based on who has a four of a kind
+            // if both players have one, the highest four of a kind wins
+            // if no four of a kind, the round will continue
+            if (playerOneThreeOfAKind && !playerTwoThreeOfAKind) {
+                winner = 'playerOne'
+                roundContinue = false
+    
+            }
+    
+            else if (!playerOneThreeOfAKind && playerTwoThreeOfAKind) {
+                winner = 'playerTwo'
+                roundContinue = false
+    
+            }
+    
+            else if (playerOneThreeOfAKind && playerTwoThreeOfAKind) {
+                if (playerOneThreeOfKindCard > playerTwoThreeOfKindCard) {
+                    winner = 'playerOne'
+                    roundContinue = false
+    
+                }
+                else if (playerOneThreeOfKindCard < playerTwoThreeOfKindCard) {
+                    winner = 'playerTwo'
+                    roundContinue = false
+    
+                }
+            }
+            
+        }
         
         
         
